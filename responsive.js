d4bb12fea3ce07c4541c401637399869b6b51f0b
javaloyes14 <javaloyes14@gmail.com>
2026-07-16 21:24:23 +0200

    Update and rename script.js to responsive.js

---
+++ responsive.js
@@ -0,0 +1,178 @@
+/* ===================================
+   RESPONSIVE
+   JERÓNIMO JAVALOYES S.L.
+=================================== */
+
+/* Tablets */
+
+@media screen and (max-width:900px){
+
+main{
+
+    width:94%;
+
+}
+
+.logo{
+
+    width:130px;
+    height:130px;
+
+}
+
+.logo img{
+
+    width:90px;
+
+}
+
+h1{
+
+    font-size:30px;
+
+}
+
+.empresa{
+
+    font-size:17px;
+
+}
+
+.card{
+
+    padding:24px;
+
+}
+
+}
+
+
+/* Móviles */
+
+@media screen and (max-width:600px){
+
+.hero{
+
+    height:300px;
+
+}
+
+.hero-wave{
+
+    bottom:-75px;
+    height:150px;
+
+}
+
+.logo{
+
+    width:115px;
+    height:115px;
+
+}
+
+.logo img{
+
+    width:80px;
+
+}
+
+h1{
+
+    font-size:27px;
+    padding:0 20px;
+
+}
+
+.empresa{
+
+    font-size:15px;
+    padding:0 20px;
+
+}
+
+.card{
+
+    border-radius:18px;
+    padding:22px;
+
+}
+
+.card h2{
+
+    font-size:22px;
+
+}
+
+.card p{
+
+    font-size:16px;
+
+}
+
+.button{
+
+    padding:15px;
+    font-size:16px;
+
+}
+
+footer{
+
+    font-size:14px;
+    padding-bottom:40px;
+
+}
+
+}
+
+
+/* Móviles pequeños */
+
+@media screen and (max-width:380px){
+
+.hero{
+
+    height:280px;
+
+}
+
+.logo{
+
+    width:100px;
+    height:100px;
+
+}
+
+.logo img{
+
+    width:70px;
+
+}
+
+h1{
+
+    font-size:23px;
+
+}
+
+.empresa{
+
+    font-size:14px;
+
+}
+
+.card{
+
+    padding:18px;
+
+}
+
+.button{
+
+    padding:14px;
+    font-size:15px;
+
+}
+
+}
